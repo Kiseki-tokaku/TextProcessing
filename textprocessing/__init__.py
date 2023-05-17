e79f01e0a408d681a8dc21b213177e1568c99e9e
Inês <66116263+Kiseki-tokaku@users.noreply.github.com>
2023-05-17 23:09:39 +0100

    Update __init__.py

--- textprocessing/__init__.py
+++ textprocessing/__init__.py
@@ -1,1 +1,8 @@
-
+from html_stripping import *
+from lemmatization import *
+from normalization import *
+from sentence_segmentation import *
+from stemming import *
+from stopwords_removal import *
+from tokenization import *
+import os
