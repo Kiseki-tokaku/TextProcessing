d901d01bf15e5cc4b7654dc959a990c91e6a67a1
Inês <66116263+Kiseki-tokaku@users.noreply.github.com>
2023-05-17 22:52:16 +0100

    Delete stemming.py

--- stemming.py
+++
@@ -1,6 +0,0 @@
-import nltk
-
-def stemming(text, language='english'):
-    stem = nltk.stem.SnowballStemmer(language)
-    text = ' '.join([stem.stem(word) for word in nltk.word_tokenize(text)])
-    return text
